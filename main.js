9ed23eac0c586dc8467ab65e182a466989982783
Martin4124 <m.ibantschitz@gmail.com>
2021-05-25 20:10:51 +0200

    Hamburger Menu

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const hamburger = document.getElementById("hamburgermenu")
 const nav = document.getElementById("nav-ul")
+const hamburgerSVG = document.getElementById("hamburger-svg")
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("show");
+    hamburgerSVG.classList.toggle("fa-bars")
+    hamburgerSVG.classList.toggle("fa-times")
 });
