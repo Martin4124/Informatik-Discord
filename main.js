f5e8f1ebe8d301b159bed1702e230a0451d1cdf7
Martin4124 <m.ibantschitz@gmail.com>
2021-05-30 21:18:38 +0200

    Revert "Hamburger menu finished"
    This reverts commit 54d820a517288a090ee98a94366a0b1dc55ace51.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const hamburger = document.getElementById("hamburgermenu")
 const nav = document.getElementById("nav-ul")
 const hamburgerSVG = document.getElementById("hamburger-svg")
 
-
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("show");
     hamburgerSVG.classList.toggle("fa-bars")
